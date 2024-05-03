afb3fe24f1fd671456350772bddf8b3c5f9afe70
Adeliya111 <134287194+Adeliya111@users.noreply.github.com>
2024-05-03 20:09:36 +0500

    Dealer.java

--- src/poker/Dealer.java
+++ src/poker/Dealer.java
@@ -1,4 +1,16 @@
 package poker;
 
 public class Dealer {
+    private final Cards cards;
+    public Dealer(Cards cards) {
+        this.cards = cards;
+    }
+
+    public void shuffleDeck() {
+        cards.shuffle();
+    }
+
+    public Card dealCard() {
+        return cards.getCard();
+    }
 }
