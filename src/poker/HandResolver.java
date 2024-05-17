f04d6b873ebb4ecdfe8342c6f87b200a04e8931b
Adeliya <adeli4ka.ab@gmail.com>
2024-05-17 19:01:48 +0500

    fix rename method

--- src/poker/HandResolver.java
+++ src/poker/HandResolver.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 public class HandResolver {
     public static Combination resolved(List<Card> cards) {
+        if(Combinations.streetFlash(cards)) {
+            return Combination.STREET_FLASH;
+        }
         if(Combinations.quads(cards)) {
             return Combination.QUADS;
+        }
+        if(Combinations.fullHouse(cards)) {
+            return Combination.FULL_HOUSE;
         }
         if(Combinations.flash(cards)) {
             return Combination.FLASH;
         }
-        if(Combinations.fullHouse(cards)) {
-            return Combination.FULL_HOUSE;
+        if(Combinations.street(cards)) {
+            return Combination.STREET;
         }
         if(Combinations.three(cards)) {
             return Combination.THREE;
@@ -21,12 +27,6 @@ public class HandResolver {
         }
         if(Combinations.pair(cards)) {
             return Combination.PAIR;
-        }
-        if(Combinations.street(cards)) {
-            return Combination.STREET;
-        }
-        if(Combinations.streetFlash(cards)) {
-            return Combination.STREET_FLASH;
         }
         return Combination.SENIOR_CARD;
     }
